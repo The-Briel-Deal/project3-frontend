4706d1c8818b9f81ba5144b3fb85886acf067a92
Dane Kim <mr.dane@danes-mbp.lan>
2022-06-17 11:16:56 -0400

    wip:added firebase to our header page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { auth } from "./components/services/firebase";
+import { useState, useEffect } from "react";
 import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Main from "./components/Layout/Main";
@@ -6,9 +8,14 @@ import "./App.css";
 const App = () => {
   const URL = "https://pipiopiproj.herokuapp.com/items/";
 
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => setUser(user));
+  });
+
   return (
     <Fragment>
-      <Header />
+      <Header user={user} />
       <div className="main-container">
         <Main URL={URL} className="container" />
       </div>
